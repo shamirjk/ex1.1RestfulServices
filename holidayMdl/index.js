2156ecc859a36e07fa75c81aacdade21202bd538
Shamir Kritzler <shamirjk@gmail.com>
2017-05-25 20:51:04 +0300

    exercise 1.1 Final Release

--- holidayMdl/index.js
+++ holidayMdl/index.js
@@ -1,12 +1,12 @@
 'use strict';
-const   data = require (`../models/movies`),
-        consts = require (`../consts`),
-        mongoose = require ('mongoose');
+const   data = require(`../models/movies`),
+        consts = require(`../consts`),
+        mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(consts.MLAB_KEY);
 
-var conn = mongoose.connection;
+var conn = mongoose.connection; //get default connection
 
 conn.on(`error`, (err) => {
     console.log(`connection error: ${err}`);
@@ -18,29 +18,40 @@ conn.once(`open`, (err) => {
     //mongoose.disconnect();
 });
 
+//return all movies in library
 exports.getAllMovies = () => {
-    return data.find({})
+    return data.find({},'-_id')
 };
 
+//return only the Movies that have a Holiday tag in it
 exports.getHolidayMovies = () => {
-    return data.find({holiday: {$exists: true}},
+    return data.find({holiday: {$exists: true}}, '-_id',
         (err) => {
             if (err) console.log(`query.error${err}`);
         });
 };
 
+//POST method that returns only the movies with specific holiday
 exports.getMoviesByHoliday = (holiday_name) => {  //POST
-    return data.find({holiday: holiday_name}, (err, list) => {
+    return data.find({holiday: holiday_name}, '-_id', (err) => {
         if (err) console.log(`query.error${err}`);
-        if (list.length < 1) {
-            return {"Error": "No Matching Movies"};
-        }
     });
 };
 
+//return all movies of specific holiday and original Language
 exports.getMoviesByHolidayAndLanguage = (holiday_name, lang) => {
-    return data.find({holiday: holiday_name, "language.original": lang},
-        (err) => {
-            if (err) console.log(`query.error${err}`);
-        });
+    let myPromise = new Promise((result) => {
+        data.find({holiday: holiday_name, "language.original": lang},'-_id',
+            (err, data) => {
+                if (err) {
+                    console.log(`query.error${err}`);
+                }
+                if (data[0] === undefined) {
+                    result({"Error": "Couldn't find Movie with Requested Parameters"});
+                } else {
+                    result(data);
+                }
+            });
+    });
+    return myPromise;
 };
--- index.js
+++ index.js
@@ -8,42 +8,50 @@ const   express= require ('express'),
 app.use(bodyParser.json()); //parsing application/JSON
 app.use(bodyParser.urlencoded({extended: true})); //parsing application
 
-app.all('*', (req, res, next) => {
-    console.log("runs for all HTTP verbs first");
-    res.status(200);
-    next();
+//Index Page that shows the API
+app.get(`/`, (req, res) => {
+    res.status(200).sendFile(__dirname + "/API/api.html")
 });
 
+//return All the Movies in Library
 app.get(`/getAllMovies`, (req, res) => {
     myList.getAllMovies().then((resData) =>
         res.status(200).json(resData)
     );
-    console.log(`get All Movies: ${req.params}`);
+    console.log(`GET Method Request All Movies`);
 });
 
+//returns all Thr movies with Holiday tag
 app.get(`/getHolidayMovies`, (req, res) => {
     myList.getHolidayMovies().then((resData) =>
         res.status(200).json(resData)
     );
-    console.log(`get Holiday Movies: ${req.params}`)
+    console.log(`GET Method Request Only Holiday Movies`)
 });
 
+//returns a movie with specific holiday and in specific Language
 app.get(`/getMoviesByHolidayAndLanguage/:holiday_name/:language`, (req, res) => {
-    myList.getMoviesByHolidayAndLanguage(req.params.holiday_name, req.params.language).then((resData) =>
-        res.status(200).json(resData)
-    );
-    console.log(`get Movies By Holiday And Language: ${req.params}`);
+        myList.getMoviesByHolidayAndLanguage(req.params.holiday_name, req.params.language).then((resData) => {
+            res.status(200).json(resData);
+        });
+    console.log(`GET Method request Movies By Holiday: ${req.params.holiday_name},
+    And Language: ${req.params.language}`);
 });
 
+//POST method that returns all movies related to a specific holiday
 app.post('/getMoviesByHoliday/', (req, res) => {
-    console.log(`POST Movies By Holiday : ${req.params}`);
+    console.log(`POST Movies By Holiday : ${req.body.holiday_name}`);
     myList.getMoviesByHoliday(req.body.holiday_name).then((data) => {
-        res.status(200).json(data);
+        if (data.length==0)
+            res.json({"Error": "No Movie with Requested Holiday"});
+        else res.status(200).json(data);
     });
 });
 
-app.get(`/`, (req, res) => {
-    res.status(200).sendFile(__dirname + "/API/api.html")
+//friendly 404 Page
+app.all('*', (req, res) => {
+    console.log(`Wrong Page address - friendly 404. Check the URL address`);
+    res.status(404).send(`Got Lost? This is a friendly 404 Page`);
 });
 
 app.listen(port, () => {//create Server
